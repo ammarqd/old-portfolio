b5a7050146329f00309db58c4e87b817f3be5c72
AmmarQ <ammar-g@hotmail.co.uk>
2024-04-18 01:53:01 +0100

    Fix typewriter effect..

--- typewriter.js
+++ typewriter.js
@@ -13,7 +13,7 @@ function typeSentence(e, delay = 50) {
 document.addEventListener("DOMContentLoaded", function() {
   const h4Element = document.querySelector('h4');
   const textLength = h4Element.innerText.length;
-  const delay = textLength * 600;
+  const delay = textLength * 50;
   h4Element.style.setProperty('--animation-delay', `${delay}ms`);
   typeSentence(h4Element);
 });
