2f6386e33faab02dfefb769453e3e4b83d52c5c1
ammarqd <ammar-g@hotmail.co.uk>
2024-07-18 00:08:19 +0100

    Clean up code

--- assets/script.js
+++ assets/script.js
@@ -1,24 +1,22 @@
-function typeSentence(e, delay = 50) {
+function typeSentence(e, typingDelay) {
   const sentence = e.innerText;
   e.innerHTML = "";
-  e.style.opacity = '1';
 
   for (let i = 0; i < sentence.length; i++) {
     setTimeout(() => {
       e.append(sentence.charAt(i));
-    }, i * delay);
+    }, i * typingDelay);
   }
 }
 
-document.addEventListener("DOMContentLoaded", function() {
+document.addEventListener("DOMContentLoaded", () => {
   const h4Element = document.querySelector('h4');
   const textLength = h4Element.innerText.length;
-  const delay = textLength * 50;
-  h4Element.style.setProperty('--animation-delay', `${delay}ms`);
-  typeSentence(h4Element);
-});
+  const typingDelay = 60
+  const blinkerDelay = textLength * typingDelay;
+  h4Element.style.setProperty('--animation-delay', `${blinkerDelay}ms`);
+  typeSentence(h4Element, typingDelay);
 
-document.addEventListener('DOMContentLoaded', () => {
   const menuToggle = document.querySelector('.menu-toggle');
   const navUl = document.querySelector('nav ul');
   const body = document.querySelector('body');
