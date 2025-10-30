1c19be198373248cffa717bc2f34afe574d718f5
ammarqd <ammarqd@outlook.com>
2025-10-30 17:03:19 +0000

    Update next.js config

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
+  basePath: '/old-portfolio',
+  assetPrefix: '/old-portfolio/',
   images: {
     unoptimized: true,
   },
+  trailingSlash: true,
 }
 
 export default nextConfig
